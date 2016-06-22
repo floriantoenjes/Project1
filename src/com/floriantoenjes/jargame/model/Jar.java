57ff504cc99d9e123cb0b1bba20f1edb4964447e
Florian Tönjes <skatepainter@yahoo.de>
2016-06-22 16:19:53 +0200

    Added a fill method.

--- src/com/floriantoenjes/jargame/model/Jar.java
+++ src/com/floriantoenjes/jargame/model/Jar.java
@@ -1,11 +1,15 @@
 package com.floriantoenjes.jargame.model;
 
 public class Jar {
-    private final String content;
-    private final int amount;
-    private final int maxAmount;
+    private String content;
+    private int amount;
+    private int maxAmount;
 
     public Jar(String content, int amount, int maxAmount) {
+        fill(content, amount, maxAmount);
+    }
+
+    public void fill(String content, int amount, int maxAmount) {
         this.content = content;
         this.amount = amount;
         this.maxAmount = maxAmount;
